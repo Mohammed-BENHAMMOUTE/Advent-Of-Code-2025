493f0d252e6bc5c8e98fbf6bb10226666300514c
Mohammed-BENHAMMOUTE <mohammedbenhammoute@gmail.com>
2025-12-27 16:57:30 +0100

    ---

--- Day_1/python/main.py
+++ Day_1/python/main.py
@@ -1,27 +1,51 @@
-def get_answer():
+def get_answer_1():
     count_zeroes = 0
-    current_pos = 50 
+    current_pos = 50
     MOD = 100
-    
+
     with open("./input.txt") as f:
-        # Use strip() to handle trailing newlines safely
         lines = [line.strip() for line in f if line.strip()]
 
     for line in lines:
         direction = line[0]
         distance = int(line[1:])
 
-        if direction == 'L':
-            # Subtract for Left. Python's % handles negatives correctly.
+        if direction == "L":
             current_pos = (current_pos - distance) % MOD
-        elif direction == 'R':
-            # Add for Right.
+        elif direction == "R":
             current_pos = (current_pos + distance) % MOD
-
-        # If it lands exactly on 0 after the move
         if current_pos == 0:
             count_zeroes += 1
 
     return count_zeroes
 
-print(get_answer())
+
+def get_answer_2():
+    count_zeroes = 0
+    current_pos = 50
+    MOD = 100
+
+    with open("./input.txt") as f:
+        lines = [line.strip() for line in f if line.strip()]
+
+    for line in lines:
+        direction = line[0]
+        distance = int(line[1:])
+
+        if direction == "L":
+            count_zeroes += distance // 100
+            rest = distance % 100
+
+            if current_pos > 0 and rest >= current_pos:
+                count_zeroes += 1
+
+            current_pos = (current_pos - distance) % MOD
+        elif direction == "R":
+            count_zeroes += (current_pos + distance) // 100
+            current_pos = (current_pos + distance) % MOD
+
+    return count_zeroes
+
+
+print(get_answer_1())
+print(get_answer_2())
